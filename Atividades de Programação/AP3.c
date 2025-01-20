18d7ba6e7baf90278bb409a3dbf18143c30aa1a1
Eduardo Lopes Arrais de Oliveira <eduardoarrais2013@gmail.com>
2025-01-20 13:04:44 -0300

    Update AP3.c

--- Atividades de Programação/AP3.c
+++ Atividades de Programação/AP3.c
@@ -4,73 +4,80 @@
 
 #define TAM_MAX 1024
 
-struct pilha {
-  int topo;
-  int itens[TAM_MAX];
-};
+typedef struct aux{
+  int num;
+  struct pilha* prox;
+}ELEMENTO;
 
-struct pilha* criar_pilha(){
-  struct pilha* nova_pilha = (struct pilha*) malloc(sizeof(struct pilha));
-  if(nova_pilha == NULL){
-    exit(-1);
-  }
-  nova_pilha->topo = -1;
-  return nova_pilha;
+typedef ELEMENTO* POINT;
+
+typedef struct {
+  POINT topo;
+}PILHA;
+
+void inicializa(PILHA* p){
+  p->topo = NULL;
 }
 
-int push(struct pilha* p, int val){
-  if(p->topo>=TAM_MAX-1){ //Verifica se a pilha está cheia
-    return -1;
+int vazia(PILHA* p){
+  if(p->topo == NULL){
+    return 1;
   }
-  (p->topo)++;
-  p->itens[p->topo] = val;
   return 0;
 }
 
-int pop(struct pilha* p){
-  if(p->topo == -1){ //Verifica se a pilha está vazia
-    return -1;
-  }
-  else{
-    int val = p->itens[p->topo];
-    (p->topo)--;
-    return val;
-  }
+void push(PILHA* p, int n){
+  POINT novo = (POINT) malloc(sizeof(ELEMENTO));
+  novo->num = n;
+  novo->prox = p->topo;
+  p->topo = novo;
 }
 
-struct pilha *remover(struct pilha* p){
-  free(p);
-  return (NULL);
+int pop(PILHA* p){
+  int n;
+  if(vazia(p) == 1) return -1;
+  n = p->topo->num;
+  POINT apagar = p->topo;
+  p->topo = p->topo->prox;
+  free(apagar);
+  return n;
 }
 
 int main(void) {
-  int i, n, x, y;
+  PILHA* minha_pilha = (PILHA*) malloc(sizeof(PILHA));
+  if(minha_pilha == NULL){
+    return -1;
+  } else{
+    inicializa(minha_pilha);
+  }
   char str[TAM_MAX];
-  struct pilha* minha_pilha = criar_pilha();
-  
+  int i, n, a1, a2, x, y;
   scanf("%[^\n]", str);
+  if(strlen(str) < 3){
+    printf("Invalid Expression"); return 0;
+  }
   for(i=0; i<strlen(str); i++){
-    if(str[i]>='0' && str[i]<='9'){
+    if(str[i] >= '0' && str[i] <= '9'){
       n = (int) str[i]-'0';
       push(minha_pilha, n);
     }
     else{
-      x = pop(minha_pilha);
-      y = pop(minha_pilha);
+      x=pop(minha_pilha); // Pega elemento do topo
+      y=pop(minha_pilha); // Pega outro elemento do topo
       if(x != -1 && y != -1){
-        switch (str[i]){
+        switch (str[i]){ // Realiza a operação utilizando a pilha
           case '+': push(minha_pilha, y + x); break;
           case '-': push(minha_pilha, y - x); break;
           case '*': push(minha_pilha, y * x); break;
           case '/': push(minha_pilha, y / x); break;
-          default: printf("Invalid Expression"); return -1;
+          default: printf("Invalid Expression"); return 0;
         }
       } else{
-        printf("Invalid Expression"); return -1;
+        printf("Invalid Expression"); return 0;
       }
     }
   }
-  printf("%d", minha_pilha->itens[minha_pilha->topo]);
-  remover(minha_pilha);
+  printf("%d", minha_pilha->topo->num);
+  free(minha_pilha);
   return 0;
 }
